65e7d58cd890e259ba9ed77fb5bf8804c16be5ba
Dom Sos <devdomsos@gmail.com>
2020-03-04 16:37:08 +0100

    added switch

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
   <div className="main-content">
     <h2>About</h2>
-    <p>The front end course directory lists many of the courses we teach on HTML, CSS, JavaScript and more! Be sure to visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
+    <p> Ea nisi consequat qui nulla dolor minim commodo sit. Anim irure exercitation officia do labore do sint officia nisi culpa magna. Elit sit mollit cillum magna laborum occaecat deserunt laborum cupidatat do velit ex ut ea. Cillum mollit ipsum culpa cupidatat nisi officia aliqua ut in laboris veniam. Id irure occaecat consectetur id. Aliquip elit fugiat mollit enim et. Ea esse deserunt occaecat ullamco Lorem adipisicing deserunt Lorem consequat dolore proident aliquip in esse. </p>
   </div>
 );
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // App components
 import Home from './Home';
@@ -7,15 +7,20 @@ import About from './About';
 import Header from './Header';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
+      {/* switch will look only for a matching routes. If it doe snot find a route that matches it fall back to "catch all routes" component NotFound  */}
+      <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/teachers" component={Teachers} />
       <Route path="/courses" component={Courses} />
+      <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
